93a5a621614fb2842be4a61143207aa363190ea7
Rob Dockins <rdockins@galois.com>
2021-09-09 16:52:49 -0700

    Start pulling the pieces together.
    Generate C code for a spec and compile it into bitcode.

---
+++ copilot-verifier/exe/Heater.hs
@@ -0,0 +1,44 @@
+--------------------------------------------------------------------------------
+-- Copyright 2019 National Institute of Aerospace / Galois, Inc.
+--------------------------------------------------------------------------------
+
+-- This is a simple example with basic usage. It implements a simple home
+-- heating system: It heats when temp gets too low, and stops when it is high
+-- enough. It read temperature as a byte (range -50C to 100C) and translates
+-- this to Celcius.
+
+module Main where
+
+import Language.Copilot
+import Copilot.Compile.C99
+--import Copilot.Core.PrettyDot (prettyPrintDot)
+--import Copilot.Language.Prelude
+
+import Copilot.Verifier (verify)
+
+import Prelude ()
+
+-- External temperature as a byte in degrees C
+temp :: Stream Word8
+temp = extern "temperature" Nothing
+
+-- width of the sliding window
+window :: Int
+window = 3
+
+-- Compute a sum of the last 3 samples
+sumTemp :: Stream Word32
+sumTemp = sum window (replicate 3 19 ++ cast temp)
+
+spec :: Spec
+spec = do
+  trigger "heaton"  (sumTemp < (18*fromIntegral window)) [arg sumTemp]
+  trigger "heatoff" (sumTemp > (21*fromIntegral window)) [arg sumTemp]
+
+-- Compile the spec
+main :: IO ()
+main = reify spec >>= verify mkDefaultCSettings "heater"
+{-
+  do spec' <- reify spec
+     putStrLn $ prettyPrintDot spec'
+-}
--- copilot-verifier/src/Copilot/Verifier.hs
+++ copilot-verifier/src/Copilot/Verifier.hs
@@ -1,1 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ImplicitParams #-}
+
 module Copilot.Verifier where
+
+import Control.Monad (foldM)
+
+import Copilot.Compile.C99 (CSettings(..), compileWith)
+import Copilot.Core
+
+-- import qualified Copilot.Theorem.What4 as CW4
+
+import Crux.LLVM.Config (llvmCruxConfig)
+import Crux.LLVM.Compile (genBitCode)
+import CruxLLVMMain
+  (withCruxLLVMLogging, cruxLLVMLoggingToSayWhat, processLLVMOptions)
+
+import Crux.Config (cfgJoin, Config(..))
+import Crux.Config.Load (fromFile, fromEnv)
+import Crux.Config.Common (cruxOptions, CruxOptions(..), postprocessOptions, )
+import Crux (defaultOutputConfig)
+import Crux.Log (cruxLogMessageToSayWhat, withCruxLogMessage)
+
+verify :: CSettings -> String -> Spec -> IO ()
+verify csettings prefix spec =
+  do compileWith csettings prefix spec
+     putStrLn ("Generated " ++ prefix)
+
+     let csrc = prefix ++ ".c"
+
+     llvmcfg <- llvmCruxConfig
+     let cfg = cfgJoin cruxOptions llvmcfg
+
+     (cruxOpts, llvmOpts) <-
+       do -- TODO, load from and actual configuration file
+          fileOpts <- fromFile "copilot-verifier" cfg Nothing
+          (cruxOpts0, llvmOpts0) <- foldM fromEnv fileOpts (cfgEnv cfg)
+          let ?outputConfig = defaultOutputConfig cruxLogMessageToSayWhat (Just cruxOpts0)
+          cruxOpts1 <- withCruxLogMessage (postprocessOptions cruxOpts0{ inputFiles = [csrc] })
+          processLLVMOptions (cruxOpts1, llvmOpts0)
+
+
+     let ?outputConfig = defaultOutputConfig cruxLLVMLoggingToSayWhat (Just cruxOpts)
+     bcOut <- withCruxLLVMLogging (genBitCode cruxOpts llvmOpts)
+
+     putStrLn ("Compiled " ++ prefix ++ " into " ++ bcOut)
