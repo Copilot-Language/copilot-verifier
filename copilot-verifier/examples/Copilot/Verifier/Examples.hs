e6dedd3e7577086da0ea17c830c748de77424eeb
Ryan Scott <rscott@galois.com>
2021-12-07 09:28:11 -0500

    Add a basic ShouldFail test
    This adds a basic test that is expected to fail due to not asserting a
    necessary precondition. We check that such tests fail in the test suite by way
    of the `tasty-expected-failure` package.
    
    Fixes #13.

--- copilot-verifier/examples/Copilot/Verifier/Examples.hs
+++ copilot-verifier/examples/Copilot/Verifier/Examples.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Copilot.Verifier.Examples (shouldPassExamples) where
+module Copilot.Verifier.Examples
+  ( shouldFailExamples
+  , shouldPassExamples
+  ) where
 
 import qualified Data.CaseInsensitive as CI
 import Data.CaseInsensitive (CI)
@@ -8,6 +11,7 @@ import Data.Map (Map)
 import Data.Text (Text)
 
 import Copilot.Verifier (Verbosity)
+import qualified Copilot.Verifier.Examples.ShouldFail.UnderConstrained as UnderConstrained
 import qualified Copilot.Verifier.Examples.ShouldPass.Array   as Array
 import qualified Copilot.Verifier.Examples.ShouldPass.Arith   as Arith
 import qualified Copilot.Verifier.Examples.ShouldPass.Clock   as Clock
@@ -19,6 +23,11 @@ import qualified Copilot.Verifier.Examples.ShouldPass.IntOps  as IntOps
 import qualified Copilot.Verifier.Examples.ShouldPass.Structs as Structs
 import qualified Copilot.Verifier.Examples.ShouldPass.Voting  as Voting
 import qualified Copilot.Verifier.Examples.ShouldPass.WCV     as WCV
+
+shouldFailExamples :: Verbosity -> Map (CI Text) (IO ())
+shouldFailExamples verb = Map.fromList
+    [ example "UnderConstrained" (UnderConstrained.verifySpec verb)
+    ]
 
 shouldPassExamples :: Verbosity -> Map (CI Text) (IO ())
 shouldPassExamples verb = Map.fromList
@@ -34,6 +43,6 @@ shouldPassExamples verb = Map.fromList
     , example "Voting" (Voting.verifySpec verb)
     , example "WCV" (WCV.verifySpec verb)
     ]
-  where
-    example :: Text -> IO () -> (CI Text, IO ())
-    example name action = (CI.mk name, action)
+
+example :: Text -> IO () -> (CI Text, IO ())
+example name action = (CI.mk name, action)
